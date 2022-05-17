1fc9dd7a3ce2811fa72ac8d69854d91afac213d8
tosinori-m <your.email@example.com>
2022-05-17 14:26:06 +0900

    calendar

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "../app.vue";
+import VCalendar from 'v-calendar';
+Vue.use(VCalendar)
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = createApp(App);
