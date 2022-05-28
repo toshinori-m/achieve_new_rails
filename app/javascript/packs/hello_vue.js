41ce80d96ddad8a827360d425c485888b6b01af9
tosinori-m <your.email@example.com>
2022-05-28 18:10:20 +0900

    calendar

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,9 +1,16 @@
 import { createApp } from "vue";
 import App from "../app.vue";
-import 'v-calendar/dist/style.css';
-
+import '../../../v-calendar/dist/style.css';
+import { SetupCalendar, Calendar, DatePicker } from 'v-calendar';
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = createApp(App);
+  const datePicker = document.getElementById('input_date');
   app.mount("#vue-app");
+  app.use(SetupCalendar, {});
+  app.component('Calendar', Calendar);
+  app.component('DatePicker', DatePicker);
+  document.getElementById('submit_btn').addEventListener('click', () => {
+    console.log(datePicker.value);
+  });
 });
